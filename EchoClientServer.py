1191c8161695ed13cc6454a9869d6139edaffbb2
Yang Xu <yangxu011900@gmail.com>
2023-03-09 14:12:52 -0500

    added encryption decryption need to add GG and GEA and error handling

--- EchoClientServer.py
+++ EchoClientServer.py
@@ -21,6 +21,8 @@ import socket
 import argparse
 import sys
 import csv
+import pandas as pd
+from cryptography.fernet import Fernet
 
 
 ########################################################################
@@ -110,6 +112,17 @@ class Server:
         # Output the socket address.
         print(client)
 
+        columns = {
+            'GL1A': 'Lab 1',
+            'GL2A': 'Lab 2',
+            'GL3A': 'Lab 3',
+            'GL4A': 'Lab 4',
+            'GMA': 'Midterm',
+        }
+
+        # encryption_keys = {
+        # }
+
         while True:
             try:
                 # Receive bytes over the TCP connection. This will block
@@ -126,23 +139,52 @@ class Server:
                     connection.close()
                     break
 
-
                 # Decode the received bytes back into strings. Then output
                 # them.
                 recvd_str = recvd_bytes.decode(Server.MSG_ENCODING)
-                print("Received " + recvd_str[8:] + " command from client")
+                ID_number = recvd_str[:7]
+                command = recvd_str[8:]
+                print("Received " + command + " command from client")
 
-                # Send the received bytes back to the client. We are
-                # sending back the raw data.
-                connection.sendall(recvd_bytes)
-                print("Sent: ", recvd_str)
+                # Check if ID matches database
+                row_num = 0
+                with open('course_grades_2023.csv', mode='r') as csv_file:
+                    read_line = csv.reader(csv_file)
+                    for row in read_line:
+                        if ID_number in row:
+                            print("User found")
+                            # print(row_num)
+                            if command in columns:
+                                print("Command found")
+                                # Reading CSV file into a DataFrame
+                                df = pd.read_csv('course_grades_2023.csv')
+
+                                # Finding the average of a column
+                                col_avg = df[columns[command]].mean()
+                                # # Debug print
+                                # print(col_avg)
+
+                                message_send = columns[command] + " : " + str(col_avg)
+                                # encode message
+                                message_bytes = message_send.encode('utf-8')
+
+                                encryption_key = df.loc[row_num - 1, 'Key']
+                                # # Debug print
+                                # print(encryption_key)
+                                encryption_key_bytes = encryption_key.encode('utf-8')
+
+                                # Encrypt the message for transmission at the server
+                                fernet = Fernet(encryption_key_bytes)
+                                encrypted_message_bytes = fernet.encrypt(message_bytes)
+                                connection.sendall(encrypted_message_bytes)
+                                print("Sent encrypted message: ", encrypted_message_bytes)
+                        row_num = row_num + 1
 
             except KeyboardInterrupt:
                 print()
                 print("Closing client connection ... ")
                 connection.close()
                 break
-
 
 
 ########################################################################
@@ -198,6 +240,11 @@ class Client:
         while True:
             # Get student ID and command
             self.input_text = input(input_message)
+            # if self.input_text[8:] != "GL1A" or "GL2A" or "GL3A" or "GL4A" or "GEA" or "GMA" or "GG":
+                # print("Command Error closing server connection ...")
+                # # that we close the socket.
+                # self.socket.close()
+
             if self.input_text != "":
                 break
 
@@ -219,33 +266,74 @@ class Client:
                 sys.exit(1)
 
     def get_ID_command(self):
-        try:
-            self.get_console_input('Please enter your ID number followed by a command: ')
-            ID_command = self.connection_send()
-            command = ID_command[8:]
-            print('Command entered: ' + command)
-            if command == 'GMA':
-                print('Getting midterm average...')
-            elif command == 'GL1A':
-                print('Getting lab 1 average...')
-            elif command == 'GL2A':
-                print('Getting lab 2 average...')
-            elif command == 'GL3A':
-                print('Getting lab 3 average...')
-            elif command == 'GL4A':
-                print('Getting lab 4 average...')
-            elif command == 'GEA':
-                print('Getting exam average...')
-            elif command == 'GG':
-                print('Getting grades...')
+        # Dictionary for ID number and key
+        encryption_key = {
+            '1803933':  'M7E8erO15CIh902P8DQsHxKbOADTgEPGHdiY0MplTuY=',
+            '1884159':  'PWMKkdXW4VJ3pXBpr9UwjefmlIxYwPzk11Aw9TQ2wZQ=',
+            '1853847':  'UVpoR9emIZDrpQ6pCLYopzE2Qm8bCrVyGEzdOOo2wXw=',
+            '1810192':  'bHdhydsHzwKdb0RF4wG72yGm2a2L-CNzDl7vaWOu9KA=',
+            '1891352':	'iHsXoe_5Fle-PHGtgZUCs5ariPZT-LNCUYpixMC3NxI=',
+            '1811313':	'IR_IQPnIM1TI8h4USnBLuUtC72cQ-u4Fwvlu3q5npA0=',
+            '1804841':	'kE8FpmTv8d8sRPIswQjCMaqunLUGoRNW6OrYU9JWZ4w=',
+            '1881925':	'_B__AgO34W7urog-thBu7mRKj3AY46D8L26yedUwf0I=',
+            '1877711':	'dLOM7DyrEnUsW-Q7OM6LXxZsbCFhjmyhsVT3P7oADqk=',
+            '1830894':	'aM4bOtearz2GpURUxYKW23t_DlljFLzbfgWS-IRMB3U=',
+            '1855191':	'-IieSn1zKJ8P3XOjyAlRcD2KbeFl_BnQjHyCE7-356w=',
+            '1821012':	'Lt5wWqTM1q9gNAgME4T5-5oVptAstg9llB4A_iNAYMY=',
+            '1844339':	'M6glRgMP5Y8CZIs-MbyFvev5VKW-zbWyUMMt44QCzG4=',
+            '1898468':	'SS0XtthxP64E-z4oB1IsdrzJwu1PUq6hgFqP_u435AA=',
+            '1883633':	'0L_o75AEsOay_ggDJtOFWkgRpvFvM0snlDm9gep786I=',
+            '1808742':	'9BXraBysqT7QZLBjegET0e52WklQ7BBYWXvv8xpbvr8=',
+            '1863450':	'M0PgiJutAM_L9jvyfrGDWnbfJOXmhYt_skL0S88ngkU=',
+            '1830190':	'v-5GfMaI2ozfmef5BNO5hI-fEGwtKjuI1XcuTDh-wsg=',
+            '1835544':	'LI14DbKGBfJExlwLodr6fkV4Pv4eABWkEhzArPbPSR8=',
+            '1820930':	'zoTviAO0EACFC4rFereJuc0A-99Xf_uOdq3GiqUpoeU=',
+        }
 
-        except (KeyboardInterrupt, EOFError):
-            print()
-            print("Closing server connection ...")
-            # If we get and error or keyboard interrupt, make sure
-            # that we close the socket.
-            self.socket.close()
-            sys.exit(1)
+        while True:
+            try:
+                self.get_console_input('Please enter your ID number followed by a command: ')
+                ID_command = self.connection_send()
+                ID = ID_command[:7]
+                command = ID_command[8:]
+                print('Command entered: ' + command)
+                if command == 'GMA':
+                    print('Getting midterm average...')
+                elif command == 'GL1A':
+                    print('Getting lab 1 average...')
+                elif command == 'GL2A':
+                    print('Getting lab 2 average...')
+                elif command == 'GL3A':
+                    print('Getting lab 3 average...')
+                elif command == 'GL4A':
+                    print('Getting lab 4 average...')
+                elif command == 'GEA':
+                    print('Getting exam average...')
+                elif command == 'GG':
+                    print('Getting grades...')
+
+                encrypted_message_bytes = self.connection_receive()
+                print("Got encrypted message: " + encrypted_message_bytes)
+                # # Debug print
+                # print(encrypted_message_bytes)
+
+
+                key = encryption_key[ID]
+                encryption_key_bytes = key.encode('utf-8')
+                # Encrypt the message for transmission at the server
+                fernet = Fernet(encryption_key_bytes)
+                # Decrypt the message after reception at the client.
+                decrypted_message_bytes = fernet.decrypt(encrypted_message_bytes)
+                decrypted_message = decrypted_message_bytes.decode('utf-8')
+                print("Decrypted_message = ", decrypted_message)
+
+            except (KeyboardInterrupt, EOFError):
+                print()
+                print("Closing server connection ...")
+                # If we get and error or keyboard interrupt, make sure
+                # that we close the socket.
+                self.socket.close()
+                sys.exit(1)
 
     def connection_send(self):
         try:
